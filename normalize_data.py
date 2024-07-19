32cb61e03b4d2021b350778c302cec26b7b516dc
lukasnxyz <lukasnitzsche@yahoo.com>
2024-07-19 10:04:12 -0500

    normalize data script class 1 as well

--- normalize_data.py
+++ normalize_data.py
@@ -9,16 +9,23 @@ with open(imgs_file, mode='r') as file:
 
 n_data = []
 data_0 = []
+data_1 = []
 for row in data:
-  if row[0] != 0.0:
-    n_data.append(row)
-  else:
-    data_0.append(row)
+  match row[0]:
+    case 0.0:
+      data_0.append(row)
+    case 1.0:
+      data_1.append(row)
+    case _:
+      n_data.append(row)
 
-rnd_idxs = np.random.randint(len(data_0), size=2000)
-
-for i in rnd_idxs:
+rnd_idxs_0 = np.random.randint(len(data_0), size=2000)
+for i in rnd_idxs_0:
   n_data.append(data_0[i])
+
+rnd_idxs_1 = np.random.randint(len(data_1), size=1900)
+for i in rnd_idxs_1:
+  n_data.append(data_1[i])
 
 output_csv = 'data/normalized_nasa_mars_images.csv'
 with open(output_csv, mode='w', newline='') as file:
